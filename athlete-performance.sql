44dbc7a877c6ec8a1785a5715138c2383347badb
Shahla-analyst <shahlarostamimanesh710@gmail.com>
2026-04-24 23:15:49 +0200

    Update athlete-performance.sql

--- athlete-performance.sql
+++ athlete-performance.sql
@@ -2,21 +2,21 @@
 
 -- 1. Total training time per athlete
 SELECT athlete, SUM(duration_min) AS total_time
-FROM workouts
+FROM dataset
+GROUP BY athlete;
+
+-- 2. Average calories burned per athlete
+SELECT athlete, AVG(calories) AS avg_calories
+FROM dataset
+GROUP BY athlete;
+
+-- 3. Max heart rate per workout type
+SELECT workout_type, MAX(heart_rate) AS max_hr
+FROM dataset
+GROUP BY workout_type;
+
+-- 4. Who trained the most?
+SELECT athlete, SUM(duration_min) AS total_time
+FROM dataset
 GROUP BY athlete
 ORDER BY total_time DESC;
-
--- 2. Average heart rate per athlete
-SELECT athlete, AVG(heart_rate) AS avg_heart_rate
-FROM workouts
-GROUP BY athlete;
-
--- 3. Total calories burned per athlete
-SELECT athlete, SUM(calories) AS total_calories
-FROM workouts
-GROUP BY athlete;
-
--- 4. Most intensive workout
-SELECT athlete, workout_type, MAX(heart_rate) AS max_hr
-FROM workouts
-GROUP BY athlete, workout_type;
